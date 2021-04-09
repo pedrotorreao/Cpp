b5865ae96c8ef1017f3c85386cf0bfb14e7035bb
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-04-08 23:33:53 -0300

    setup Account base class

--- content/inheritance/bank-account/Account.cpp
+++ content/inheritance/bank-account/Account.cpp
@@ -1,1 +1,38 @@
 #include "Account.h"
+
+Account::Account(std::string userName, double userBalance)
+    : acc_user{userName}, acc_balance{userBalance}
+{
+}
+
+bool Account::deposit(double amountToDeposit)
+{
+  if (amountToDeposit < 0)
+  {
+    return false;
+  }
+  acc_balance += amountToDeposit;
+  return true;
+}
+
+bool Account::withdraw(double amountToWithdraw)
+{
+  if (acc_balance - amountToWithdraw < 0)
+  {
+    return false;
+  }
+  acc_balance -= amountToWithdraw;
+  return true;
+}
+
+double Account::getBalance() const
+{
+  return acc_balance;
+}
+
+std::ostream &operator<<(std::ostream &os, const Account &accountObject)
+{
+  os << "[Account holder: " << accountObject.acc_user << " - Balance: " << accountObject.acc_balance << "]"
+     << "\n";
+  return os;
+}
--- content/inheritance/bank-account/Account.h
+++ content/inheritance/bank-account/Account.h
@@ -2,3 +2,22 @@
 
 #include <iostream>
 #include <string>
+
+class Account
+{
+  friend std::ostream &operator<<(std::ostream &os, const Account &accountObject);
+
+private:
+  static constexpr const char *default_name = "Unused/Vacant Account";
+  static constexpr double default_balance = 0.0;
+
+protected:
+  std::string acc_user;
+  double acc_balance;
+
+public:
+  Account(std::string acc_user = default_name, double acc_balance = default_balance);
+  bool deposit(double amountToDeposit);
+  bool withdraw(double amountToWithdraw);
+  double getBalance() const;
+}
