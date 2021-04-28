e0f19378f736f79289b7e95a716e2f93becbcde2
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-04-27 22:39:39 -0300

    add account classes utility methods

--- content/inheritance/bank-account/Account_Utilities.h
+++ content/inheritance/bank-account/Account_Utilities.h
@@ -3,3 +3,13 @@
 #include <vector>
 #include "Account.h"
 #include "Savings_Account.h"
+
+// utility functions for Account class:
+void display(const std::vector<Account> &accounts);
+void deposit(std::vector<Account> &accounts, double amountToDeposit);
+void withdraw(std::vector<Account> &account, double amountToWithdraw);
+
+// utility functions for Savings_Account class:
+void display(const std::vector<Savings_Account> &accounts);
+void deposit(std::vector<Savings_Account> &accounts, double amountToDeposit);
+void withdraw(std::vector<Savings_Account> &account, double amountToWithdraw);
