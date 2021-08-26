e97f5a1bc172990c7bd4242823d47a613e0bcc76
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-08-25 23:24:55 -0300

    [add] io-streams challenge - setup + initial data

---
+++ content/io-streams/io-streams-practice/tour_info.h
@@ -0,0 +1,26 @@
+// Title: tour_info.h
+// Desc.: file contains the structure definifitions for the tour data.
+#pragma once
+
+#include <vector>
+#include <string>
+
+struct City
+{
+  std::string name;
+  long population;
+  double cost;
+};
+
+// to make it easier, just assume each country has at least 1 city:
+struct Country
+{
+  std::string name;
+  std::vector<City> cities;
+};
+
+struct Tours
+{
+  std::string title;
+  std::vector<Country> countries;
+};
