1a1845a54064791e48bec8b7f91c3713e2c6584e
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2021-09-14 05:41:53 -0300

    add challenge description

--- content/io-streams/read-files-practice/main.cpp
+++ content/io-streams/read-files-practice/main.cpp
@@ -1,3 +1,9 @@
+/* -- Challenge description:
+Write a program that reads a file named "responses.txt" that contains the answer key for a quiz
+as well as student responses for the quiz. You should read the file and display each student's 
+name and score (#correct out of 5). At the end, the class average should be displayed. You may 
+assume that the data in the file is properly formatted
+*/
 
 #include <iostream>
 #include <string>
