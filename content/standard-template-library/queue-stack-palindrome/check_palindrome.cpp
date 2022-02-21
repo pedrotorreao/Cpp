e852d9a34febae47d35c7364a0a04d52f27f5ee1
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2022-02-20 21:39:33 -0300

    finish palindrome check using stack

--- content/standard-template-library/queue-stack-palindrome/check_palindrome.cpp
+++ content/standard-template-library/queue-stack-palindrome/check_palindrome.cpp
@@ -1,5 +1,6 @@
 // standard headers:
 #include <iostream>
+#include <stack>
 #include <string>
 
 // user-defined headers:
@@ -7,8 +8,41 @@
 #include "util.h"
 
 bool is_palindrome(const std::string &str) {
-  bool res{false};
-  // .. solution
+  std::stack<char> st;
+  std::string str_cp{str};
 
-  return res;
+  // clean the string to only leave characters
+  // from a-z and A-Z:
+  clean_line(str_cp);
+
+  int mid = str_cp.size() / 2;
+  int i{0};
+
+  // push half the characters into the stack:
+  while (i < mid) {
+    st.push(str_cp.at(i));
+    ++i;
+  }
+
+  // in case the stack is odd sized, skip the
+  // middle character:
+  if (str_cp.size() % 2 != 0)
+    ++i;
+
+  // keep iterating through the string while popping
+  // the characters from the stack and comparing them.
+  // if they mismatch, the string is not a palindrome,
+  // return false:
+  while (i < str_cp.size()) {
+    if (str_cp.at(i) != st.top()) {
+      return false;
+    }
+
+    st.pop();
+    ++i;
+  }
+
+  // otherwise, all the elements will be popped and the
+  // string is a palindrome, return true:
+  return true;
 }
